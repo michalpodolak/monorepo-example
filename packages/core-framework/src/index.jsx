34c9d33c6ea3aaa44012d466e09b46152d449dfe
Michal <michal.podolak0@gmail.com>
2019-08-13 12:26:43 +0200

    very big change to core-framework

--- packages/core-framework/src/index.jsx
+++ packages/core-framework/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Heading = () => <div>Heading comming from core-framework package change 6</div>;
+const Heading = () => <div>Heading comming from core-framework package change 7</div>;
 
 export default {
   components: { Heading },
